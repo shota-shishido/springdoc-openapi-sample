e2540e505f601ad275d5e26c4eb5384592e08261
shota-shishido <76931201+shota-shishido@users.noreply.github.com>
2024-09-18 16:45:03 +0900

    CreateUserRequest修正

--- src/main/kotlin/com/example/demo/model/CreateUserRequest.kt
+++ src/main/kotlin/com/example/demo/model/CreateUserRequest.kt
@@ -4,12 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(name = "CreateUserRequest", description = "作成する利用者の情報")
 data class CreateUserRequest(
-    @field:Schema(description = "ユーザー名", example = "John Doe", required = true)
+    @Schema(description = "ユーザー名", example = "John Doe", required = true)
     val name: String,
 
-    @field:Schema(description = "ユーザーの年齢", example = "30", minimum = "0", maximum = "150")
+    @Schema(description = "ユーザーの年齢", example = "30", minimum = "0", maximum = "150")
     val age: Int,
 
-    @field:Schema(description = "メールアドレス", example = "john@example.com")
+    @Schema(description = "メールアドレス", example = "john@example.com")
     val email: String
 )
